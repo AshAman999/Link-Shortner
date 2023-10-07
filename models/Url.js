57779d5e5108e09ab5a54832b7b883b1d11a0e5e
Aman Raj <amanpie9@gmail.com>
2023-10-07 10:02:58 +0530

    mvp created

---
+++ models/Url.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const ClickSchema = new mongoose.Schema({
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const UrlSchema = new mongoose.Schema({
+  urlId: {
+    type: String,
+    required: true,
+  },
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+  },
+  clicks: [ClickSchema], // Array of click timestamps
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  totalClicks: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export default mongoose.model("Url", UrlSchema);
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import Url from "../models/Url.js";
+const router = express.Router();
+
+router.post("/check", (req, res) => {
+  res.send(req.body);
+});
+
+router.get("/:urlId", async (req, res) => {
+  try {
+    const url = await Url.findOne({ urlId: req.params.urlId });
+    if (url) {
+      // Add the click timestamp to the clickHistory array
+      const clickTimestamp = new Date();
+      url.clicks.push({ timestamp: clickTimestamp });
+
+      // Increment the totalClicks count
+      url.totalClicks += 1;
+
+      await url.save();
+
+      return res.redirect(url.originalUrl);
+    } else {
+      return res.status(404).json("URL not found");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json("Server Error");
+  }
+});
+
+export default router;
